120899cf3a5c17d6fa07ff6d1e09fa13cfa05f9d
lihi <lihibloy327@gmail.com>
2023-09-13 16:32:46 +0300

    main

---
+++ ExmpleGit (1).py
@@ -0,0 +1,15 @@
+#1
+print(" Hello Enter your  name: dog ")
+
+#2
+print(" Hello Enter your adress: ")
+
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
+
+print("Lihi")
+print("Jerusalem")
